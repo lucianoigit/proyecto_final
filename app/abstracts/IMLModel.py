8494ef674a1b6e582025d412a993c523f46ba8a9
Luciano Iriarte <lucianoiriartegit@gmail.com>
2024-11-28 23:15:28 -0300

    fix iaService

--- app/abstracts/IMLModel.py
+++ app/abstracts/IMLModel.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 
 class MLModelInterface(ABC):
     @abstractmethod
-    def load_model(self):
+    def load_model(self,model_path):
         pass
 
     @abstractmethod
-    def run_model(self, message,roi):
+    def run_model(self, message,confianza,roi):
         pass
 
 
