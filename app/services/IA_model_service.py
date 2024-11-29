9a5553943339e1d06960b9d5ae00d23558368a41
Luciano Iriarte <lucianoiriartegit@gmail.com>
2024-11-29 03:34:15 -0300

    change yolo model

--- app/services/IA_model_service.py
+++ app/services/IA_model_service.py
@@ -11,8 +11,8 @@ class MLModelService(MLModelInterface):
     def load_model(self):
         try:
             # Cargar el modelo YOLOv5 personalizado
-            model = YOLO(self.model_path)
-            model.export(format="ncnn")
+            """             model = YOLO(self.model_path)
+            model.export(format="ncnn") """
             # creates 'yolo11n_ncnn_model'
             # Load the exported NCNN model
             ncnn_model = YOLO("yolo11n_ncnn_model")
